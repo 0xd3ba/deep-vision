89ea27ca49281bb83e7ee705044c95bde03fcaad
Debasish Das <d3ba.1996@gmail.com>
2021-07-08 03:17:30 +0530

    add alexnet

---
+++ arch/AlexNet/alexnet.py
@@ -0,0 +1,64 @@
+# alexnet.py -- Module containing the class for the AlexNet model
+
+import torch
+import torch.nn as nn
+
+
+class AlexNet(nn.Module):
+    """
+    Module for the AlexNet model with ImageNette (320px) dataset (ImageNet is too big: ~155GB !! )
+    Paper: https://papers.nips.cc/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf
+
+    NOTE: - The dimensions for each image is (3, 227, 227)
+            The paper mentions them as (3, 224, 224) which is a mistake
+
+          - The original ImageNet dataset has 1000 classes. ImageNette-320, which is used here,
+            has 10 classes. So output layer will have only 10 units, instead of 1000.
+    """
+
+    n_classes = 10      # The number of classes in the dataset that is being used
+
+    def __init__(self):
+        super().__init__()
+
+        self.conv_layers = [
+            nn.Conv2d(3, 96, kernel_size=(11, 11), stride=(4, 4)),    # Output size: (96, 55, 55)
+            nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2)),          # Output size: (96, 27, 27)
+            nn.Conv2d(96, 256, kernel_size=(5, 5), padding='same'),   # Output size: (256, 27, 27)
+            nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2)),          # Output size: (256, 13, 13)
+
+            # Now comes convolutions without any max-pooling
+            # The shape remains the same however
+            nn.Conv2d(256, 384, kernel_size=(3, 3), padding='same'),  # Output size: (384, 13, 13)
+            nn.Conv2d(384, 384, kernel_size=(3, 3), padding='same'),  # Output size: (384, 13, 13)
+            nn.Conv2d(384, 256, kernel_size=(3, 3), padding='same'),  # Output size: (256, 13, 13)
+
+            # The final max-pool layer
+            nn.MaxPool2d(kernel_size=(3, 3), stride=(2, 2))           # Output size: (256, 6, 6)
+        ]
+
+        # Each sample is of shape (256*6*6, ) = (9216, )
+        self.fc_layers = [
+            nn.Linear(9216, 4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),      # AlexNet uses dropout with probability 0.5
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, self.n_classes)
+        ]
+
+        self.conv_net = nn.Sequential(*self.conv_layers)
+        self.fc_net = nn.Sequential(*self.fc_layers)
+
+    def forward(self, X):
+        """
+        X: (batch_size, 3, 227, 227)
+        """
+        batch_size = X.shape[0]
+        y = self.conv_net(X)
+        y = y.reshape(batch_size, -1)   # Reshape to match input size of fully connected layers
+        y = self.fc_net(y)
+
+        # Not using Softmax activation because Cross-Entropy loss requires un-normalized scores
+        return y
---
+++ arch/AlexNet/main.py
@@ -0,0 +1,49 @@
+# main.py -- Entry point for training/testing LeNet-5 model
+
+import torch
+from torch.utils.data.dataloader import DataLoader
+
+# Custom modules -- the dataset class, model class and the training/testing functions
+from dataset.imagenette import ImageNetteDataset
+from alexnet import AlexNet
+from training import train
+
+
+###############################################
+# Model input and output shapes
+# Since we're using MNIST, the images
+# are 32x32, and there are 10 classes
+INPUT_DIM = 28
+OUTPUT_DIM = 10
+
+# The dataset related parameters. Extract the
+# dataset and place it wherever you wish
+# Just set the directory to it appropriately
+DATASET_DIR = './dataset/imagenette2-320/'
+ANNOTATION_CSV = 'noisy_imagenette.csv'
+
+BATCH_SIZE = 32
+###############################################
+
+
+def prepare_dataset(train=True):
+    """ Loads the dataset and return the data-loader of it """
+
+    dataset = ImageNetteDataset(data_dir=DATASET_DIR, annotation_csv=ANNOTATION_CSV, train=train)
+    data_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    return data_loader
+
+
+if __name__ == '__main__':
+
+    train_loader = prepare_dataset(train=True)
+
+    # NOTE: Torch represents images as (Channels, Height, Width)
+    #       Each sample in the train/test loaders are hence (batch_size, 3, 224, 224)
+    # Create the model and train it
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = AlexNet()
+
+    train(train_loader, model, device)
